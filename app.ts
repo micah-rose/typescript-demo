db7e74480fed6f077171eb818a93f3c88dee8690
Micah Lund <micah.r.lund@gmail.com>
2021-01-22 12:03:10 -0700

    Added a post route for pushing a new to do item to the todos array.

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import todosRoutes from './routes/todos';
 
 const app = express();
+
+app.use(bodyParser.json());
 
 app.use(todosRoutes);
 
--- routes/todos.ts
+++ routes/todos.ts
@@ -9,4 +9,13 @@ router.get('/', (req, res, next) => {
     res.status(200).json( {todos: todos})
 })
 
+router.post('/todo', (req, res, next) => {
+    const newTodo: Todo = {
+        id: new Date().toISOString(),
+        text: req.body.text
+    };
+
+    todos.push(newTodo);
+})
+
 export default router;
